fe574a5e9513cac1c83185f99e4959bf8d5cad6d
hamza1592 <1592hamza@gmail.com>
2016-08-24 01:35:21 +0500

    js for loading of images only when project is opened.
    This uses the onload function and appends the image when available.

--- js/custom.js
+++ js/custom.js
@@ -12,9 +12,9 @@ function rotateSpan(elem){
 
 
 function initializeCustomImgSlider(currentModal){
-  var x = $('#'+currentModal+ ' .sliderImages');
-  if(x != null && x.length > 0)
-  	x[0].style.display = "inline";
+	var x = $('#'+currentModal+ ' .sliderImages');
+	if(x != null && x.length > 0)
+		x[0].style.display = "inline";
 }
 
 function showNextImage(currentModal){
@@ -36,20 +36,57 @@ function showPrevImage(currentModal){
 
 
 function showImage(currentModal,n) {
-  var elem = $("#"+currentModal + " input[name='imageSliderIndex']");
-  var i;
-  var x = $('#'+currentModal+ ' .sliderImages');
-  if(x == null || x.length<=0)
-  	return;
-  if (n > x.length) {
-  	elem.val("1");
-  }
-  if (n < 1) {
-  	elem.val(x.length);
-  }
-  var slideIndex = parseInt(elem.val());
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "inline";
+	var elem = $("#"+currentModal + " input[name='imageSliderIndex']");
+	var i;
+	var x = $('#'+currentModal+ ' .sliderImages');
+	if(x == null || x.length<=0)
+		return;
+	if (n > x.length) {
+		elem.val("1");
+	}
+	if (n < 1) {
+		elem.val(x.length);
+	}
+	var slideIndex = parseInt(elem.val());
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex-1].style.display = "inline";
+}
+
+
+function showModal(modalId,postId,category,imageArray){
+	$("#progressBar").show();
+	var imagesArea = $(modalId+" .image-container-div");
+	if($.trim($(imagesArea).html()).length == 0){
+		var imgPath = "img/portfolio/" + category +"/" + postId +"/"; 
+		for(var i=0;i<imageArray.length;i++){
+			var img = $("<img />").css({"width":imageArray[i].width,"height":imageArray[i].height,"display":"none"})
+			.addClass("sliderImages img-responsive img-centered").attr('src', imgPath + imageArray[i].name)
+			.on('load', function() {	
+				if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+					alert('broken image!');
+				} else {
+					if($.trim($(imagesArea).html()).length == 0)
+						$(this).css("display","inline");
+					$(imagesArea).append($(this));
+				}
+			});
+		}
+	}
+	$("#progressBar").hide();
+	$(modalId).modal();
+
+}
+
+function formatImageStr(str){
+	var re = new RegExp('=>', 'g');
+	str = str.replace(re, ':');
+	re = new RegExp('}{','g');
+	str = str.replace(re,'},{');
+	re = new RegExp('nil','g');
+	str = str.replace(re,'null');
+	var imageJSON = JSON.parse(str);
+	return imageJSON;
+
 }
